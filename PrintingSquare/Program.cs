1e6d841ad247058b38adc0776ddcdf2b05182f7f
Alexander <pchp250g8@tutamail.com>
2026-01-28 10:57:59 +0300

    Добавьте файлы проекта.

---
+++ PrintingSquare/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+var strLine = "Hello,World!!! Printing Square!!!";
+Console.Clear();
+Console.CursorVisible = false;
+for(int i = 0; i < strLine.Length; i++)
+{
+    Console.SetCursorPosition(10 + i, 10);
+    Console.Write(strLine[i]+"\u2588");
+    Thread.Sleep(500);
+}
+Console.SetCursorPosition(10, 10);
+Console.Write(strLine+" ");
+Console.ReadKey();
+
